6f82240d0f3dc1e93109d9439658979547533299
German Cruz <cruzgerman216@yahoo.com>
2020-09-30 18:37:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+// Your code here
+function createEmployeeRecord(arr){
+  return {firstName:arr[0], familyName: arr[1], title:arr[2], payPerHour:arr[3], timeInEvents:[], timeOutEvents: []};
+}
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
